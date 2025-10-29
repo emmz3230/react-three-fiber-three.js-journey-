0659730b15099c183553973db7241e44295e50be
emmz3230 <emmz3230@gmail.com>
2025-10-29 11:58:26 +0100

    mouse event done

--- src/Mouse-events/Experience.js
+++ src/Mouse-events/Experience.js
@@ -1,13 +1,23 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {
+  meshBounds,
+  OrbitControls,
+  useGLTF,
+} from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
   const cube = useRef();
+  const hamburger = useGLTF("./hamburger.glb");
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
   });
+  const eventHandler = () => {
+    cube.current.material.color.set(
+      `hsl(${Math.random() * 360}, 100%,75%)`
+    );
+  };
 
   return (
     <>
@@ -21,7 +31,19 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh ref={cube} position-x={2} scale={1.5}>
+      <mesh
+        ref={cube}
+        raycast={meshBounds}
+        position-x={2}
+        scale={1.5}
+        onClick={eventHandler}
+        onPointerEnter={() => {
+          document.body.style.cursor = "pointer";
+        }}
+        onPointerLeave={() => {
+          document.body.style.cursor = "default";
+        }}
+      >
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
@@ -30,6 +52,14 @@ export default function Experience() {
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
+      <primitive
+        object={hamburger.scene}
+        scale={0.25}
+        position-y={0.5}
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      />
     </>
   );
 }
