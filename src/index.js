001b2ea85831a48feb9c9d73b6db228ae057a553
emmz3230 <emmz3230@gmail.com>
2025-11-02 16:05:30 +0100

    starter file for a game

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import Experience from "./r3f-physics/Experience";
+import Experience from "./r3f-game/Experience";
 
 const root = createRoot(document.querySelector("#root"));
 root.render(
